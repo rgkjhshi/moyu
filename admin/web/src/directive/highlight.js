7b62ae5b1dbbf288d78c3df7207ece3723a59bdc
石松 <rgkjhshi@gmail.com>
2023-12-22 16:20:16 +0800

    Update highlight.js

--- admin/web/src/directive/highlight.js
+++ admin/web/src/directive/highlight.js
@@ -1,4 +1,4 @@
-// highlight.js  代码高亮指令
+// highlight.js 代码高亮指令(这是自定义的插件)
 import Highlight from 'highlight.js' // 导入代码高亮文件
 import 'highlight.js/styles/a11y-light.css' // 代码高亮风格，选择更多风格需导入 node_modules/hightlight.js/styles/ 目录下其它css文件
 
