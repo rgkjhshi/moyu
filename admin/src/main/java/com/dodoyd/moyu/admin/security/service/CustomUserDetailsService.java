0e8b7a593aa3e153be03e3ef41500a513e9ac0c5
石松 <rgkjhshi@gmail.com>
2024-01-04 09:45:32 +0800

    Update CustomUserDetailsService.java

--- admin/src/main/java/com/dodoyd/moyu/admin/security/service/CustomUserDetailsService.java
+++ admin/src/main/java/com/dodoyd/moyu/admin/security/service/CustomUserDetailsService.java
@@ -1,10 +1,15 @@
 package com.dodoyd.moyu.admin.security.service;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 用户信息加载服务的自定义实现类
@@ -18,10 +23,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 权限
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_ADMIN");
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(authority);
         // Load user from database or other data source
-        User user = null;
-//        userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        User user = new User("admin", "admin", authorities);
         // Create UserDetails object
-        return null;
+        return user;
     }
 }
