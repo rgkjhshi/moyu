497ac10fcabd812115a398e42534a04d6ea7cfc7
石松 <rgkjhshi@gmail.com>
2024-01-05 19:05:55 +0800

    Update CustomUserDetailsService.java

--- admin/src/main/java/com/dodoyd/moyu/admin/security/service/CustomUserDetailsService.java
+++ admin/src/main/java/com/dodoyd/moyu/admin/security/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.dodoyd.moyu.admin.security.service;
 
+import com.dodoyd.moyu.admin.domain.SysUser;
 import com.dodoyd.moyu.admin.model.LoginUser;
+import com.dodoyd.moyu.admin.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,10 +27,14 @@ import java.util.List;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Resource
+    private SysUserService sysUserService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("用户验证:{}", username);
         // 从数据库获取用户
+        SysUser sysUser = sysUserService.querySysUserByUsername(username);
 
         // 获取授权列表
         List<GrantedAuthority> authorities = new ArrayList<>();
@@ -35,7 +42,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_ADMIN");
         authorities.add(authority);
         // Load user from database or other data source
-        String password = new BCryptPasswordEncoder().encode("admin");
+        String password = new BCryptPasswordEncoder().encode(sysUser.getUserPwd());
         LoginUser loginUser = new LoginUser(username, password, authorities);
         // Create UserDetails object
         return loginUser;
