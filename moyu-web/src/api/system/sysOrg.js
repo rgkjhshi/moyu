330b42441e669ae380c896e59cbdc5c01482b1f0
shisong <rgkjhshi@gmail.com>
2024-11-28 16:54:10 +0800

    Update sysOrg.js

--- moyu-web/src/api/system/sysOrg.js
+++ moyu-web/src/api/system/sysOrg.js
@@ -1,9 +1,15 @@
 import request from '@/utils/request'
 
-// 获取组织机构树形选项
+// 获取组织树下拉选项
 export function getOrgTreeOptions() {
   return request({
     url: '/api/system/org/tree',
     method: 'get'
   })
 }
+
+// 分页获取组织列表
+export function getOrgList(data) {
+  return request.postJson('/api/system/org/page', data)
+}
+
