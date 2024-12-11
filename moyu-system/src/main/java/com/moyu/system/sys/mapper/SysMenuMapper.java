f8dbaa6eb5eb827dd63e2fdb295b9bc0ef28f768
shisong <rgkjhshi@gmail.com>
2024-12-11 10:37:04 +0800

    Update SysMenuMapper.java

--- moyu-system/src/main/java/com/moyu/system/sys/mapper/SysMenuMapper.java
+++ moyu-system/src/main/java/com/moyu/system/sys/mapper/SysMenuMapper.java
@@ -1,14 +1,16 @@
 package com.moyu.system.sys.mapper;
 
-import com.moyu.system.sys.model.entity.SysMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.moyu.system.sys.model.entity.SysMenu;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
-* @author shisong
-* @description 针对表【sys_menu(菜单权限表)】的数据库操作Mapper
-* @createDate 2024-12-10 21:05:13
-* @Entity com.moyu.system.sys.model.entity.SysMenu
-*/
+ * @author shisong
+ * @description 针对表【sys_menu(菜单权限表)】的数据库操作Mapper
+ * @createDate 2024-12-10 21:05:13
+ * @Entity com.moyu.system.sys.model.entity.SysMenu
+ */
+@Mapper
 public interface SysMenuMapper extends BaseMapper<SysMenu> {
 
 }
