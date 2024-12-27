f3030cf3e27ceb540fa3390626d8cb9cd535897f
shisong <rgkjhshi@gmail.com>
2024-12-27 19:43:08 +0800

    wtTokenAuthenticationFilter

--- moyu-system/src/main/java/com/moyu/system/auth/config/SecurityConfig.java
+++ moyu-system/src/main/java/com/moyu/system/auth/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.moyu.system.auth.config;
 
 
+import com.moyu.system.auth.security.filter.JwtTokenAuthenticationFilter;
+import com.moyu.system.auth.security.handle.AuthenticationEntryPointImpl;
+import com.moyu.system.auth.security.handle.LogoutSuccessHandlerImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.annotation.Resource;
 
@@ -24,17 +31,53 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private UserDetailsService userDetailsService;
 
-//    @Resource
-//    private JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter;
-//
-//    @Resource
-//    private AuthenticationEntryPointImpl authenticationEntryPoint;
-//
-//    @Resource
-//    private LogoutSuccessHandlerImpl logoutSuccessHandler;
+    @Resource
+    private JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter;
+
+    @Resource
+    private AuthenticationEntryPointImpl authenticationEntryPoint;
+
+    @Resource
+    private LogoutSuccessHandlerImpl logoutSuccessHandler;
+
+
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 设置跨域访问可以携带cookie
+        config.setAllowCredentials(true);
+        // 设置访问源地址
+        config.addAllowedOriginPattern("*");
+        // 设置访问源请求头 允许携带任何头信息
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法 允许所有的请求方法
+        config.addAllowedMethod("*");
+        // 初始化cors配置源对象
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 给配置源对象设置过滤的参数 == > 所有的路径都要求校验是否跨域
+        source.registerCorsConfiguration("/**", config);
+        // 返回配置好的CorsFilter
+        return new CorsFilter(source);
+    }
 
     /**
      * Security配置
+     * anyRequest          |   匹配所有请求路径
+     * access              |   SpringEl表达式结果为true时可以访问
+     * anonymous           |   匿名可以访问
+     * denyAll             |   用户不能访问
+     * fullyAuthenticated  |   用户完全认证可以访问（非remember-me下自动登录）
+     * hasAnyAuthority     |   如果有参数，参数表示权限，则其中任何一个权限可以访问
+     * hasAnyRole          |   如果有参数，参数表示角色，则其中任何一个角色可以访问
+     * hasAuthority        |   如果有参数，参数表示权限，则其权限可以访问
+     * hasIpAddress        |   如果有参数，参数表示IP地址，如果用户IP和参数匹配，则可以访问
+     * hasRole             |   如果有参数，参数表示角色，则其角色可以访问
+     * permitAll           |   用户可以任意访问
+     * rememberMe          |   允许通过remember-me登录的用户访问
+     * authenticated       |   用户登录后可访问
      */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -42,17 +85,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity.authorizeRequests();
 //        permitAllUrl.getUrls().forEach(url -> registry.antMatchers(url).permitAll());
 
+        // CSRF禁用，否则POST工具报403错误
+        httpSecurity.csrf().disable();
+        // 允许跨域访问
+        httpSecurity.cors();
+
+        //不使用默认退出，自定义退出
+        httpSecurity.logout().disable();
+
         httpSecurity
-                // CSRF禁用，因为不使用session
-                .csrf().disable()
-                // 允许跨域访问
                 .cors()
                 // 表单登录处理器地址
 //                .and().formLogin().loginProcessingUrl("/api/login")
                 // 禁用HTTP响应头的缓存控制。默认情况下，会添加一些缓存控制头部，如no-store和private，以确保敏感数据不会被缓存。
                 .and().headers().cacheControl().disable()
                 // 认证失败处理
-//                .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+                .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
                 // 设置会话会话创建策略为无状态, 基于token，不使用session
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 // 认证请求配置
@@ -67,22 +115,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
         // 添加Logout处理器
-//        httpSecurity.logout().logoutUrl("/api/logout").logoutSuccessHandler(logoutSuccessHandler);
+        httpSecurity.logout().logoutUrl("/api/logout").logoutSuccessHandler(logoutSuccessHandler);
         // 添加JWT filter
-//        httpSecurity.addFilterBefore(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     /**
-     * 认证管理器配置
+     * 身份认证管理器配置
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // 设置自定义身份认证接口进行身份认证，并使用BCryptPasswordEncoder进行密码加密。
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     /**
-     * AuthenticationManager作为Bean声明，使用时可直接注入
+     * 身份认证管理器 AuthenticationManager作为Bean声明，使用时可直接注入
      */
     @Bean
     @Override
@@ -90,4 +138,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * 密码编码器(修改、重置密码使用)
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
---
+++ moyu-system/src/main/java/com/moyu/system/auth/security/handle/LogoutSuccessHandlerImpl.java
@@ -0,0 +1,47 @@
+package com.moyu.system.auth.security.handle;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moyu.common.model.BaseResponse;
+import com.moyu.system.auth.model.LoginUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author shisong
+ * @since 2024-12-27
+ */
+@Slf4j
+@Service
+public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
+
+    @Resource
+    private TokenService tokenService;
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        LoginUser loginUser = null;//tokenService.getLoginUser(request);
+        if (loginUser != null) {
+            // 删除用户缓存记录
+//            tokenService.removeLoginUser(loginUser.getToken());
+            // 记录用户退出日志
+            log.info("退出登录:{}", loginUser.getUsername());
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().print(new ObjectMapper().writeValueAsString(BaseResponse.getSuccessResponse("退出成功")));
+    }
+}
