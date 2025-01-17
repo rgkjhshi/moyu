4e7112279619bb90acd187229d282624185a06df
shisong <rgkjhshi@gmail.com>
2025-01-17 23:07:44 +0800

    Update SysRelationParam.java

--- moyu-system/src/main/java/com/moyu/system/sys/model/param/SysRelationParam.java
+++ moyu-system/src/main/java/com/moyu/system/sys/model/param/SysRelationParam.java
@@ -55,6 +55,7 @@ public class SysRelationParam {
 
     /**
      * 关系类型(字典 1:group_has_user,2:group_has_role,3:role_has_menu)
+     * @see com.moyu.system.sys.enums.RelationTypeEnum
      */
     private Integer relationType;
 
