424ad5bf3eb61faf01ee3359ed11b35114c5bff5
shisong <rgkjhshi@gmail.com>
2025-01-24 01:26:36 +0800

    Create JwtTokenAuthenticationFilter.java

---
+++ moyu-common/common-security/src/main/java/com/moyu/common/security/filter/JwtTokenAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.moyu.common.security.filter;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.moyu.common.security.model.LoginUserDetails;
+import com.moyu.common.security.service.TokenService;
+import com.nimbusds.jwt.JWTClaimsSet;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * JWT令牌认证过滤器，用于从请求头中获取令牌并进行验证。
+ *
+ * @author shisong02
+ * @link <a href="https://mikechen.cc/30272.html">Security工作流程参考这里</a>
+ * @since 2024-01-04
+ */
+@Slf4j
+@Service
+public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
+
+    @Resource
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 获取请求携带的令牌
+        String token = TokenService.getTokenFromRequest(request);
+        if (ObjectUtil.isNotEmpty(token)) {
+            // 验证token，并解析为jwt对象
+            // 从缓存中获取用户信息
+            LoginUserDetails userDetails = TokenService.getLoginUserByToken(token);
+            // 如果 loginUser 存在，设置SecurityContext
+            if (ObjectUtil.isNotNull(userDetails) && ObjectUtil.isEmpty(SecurityContextHolder.getContext().getAuthentication())) {
+                // 设置认证对象
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        // 继续执行后续的过滤器链
+        filterChain.doFilter(request, response);
+    }
+}
