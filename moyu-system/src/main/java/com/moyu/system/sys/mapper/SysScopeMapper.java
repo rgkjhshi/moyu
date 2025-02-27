e2a242425c03a6fcc1e78a3384cbe30e7e630624
shisong <rgkjhshi@gmail.com>
2025-02-27 11:01:29 +0800

    Update SysScopeMapper.java

--- moyu-system/src/main/java/com/moyu/system/sys/mapper/SysScopeMapper.java
+++ moyu-system/src/main/java/com/moyu/system/sys/mapper/SysScopeMapper.java
@@ -1,14 +1,16 @@
 package com.moyu.system.sys.mapper;
 
-import com.moyu.system.sys.model.entity.SysScope;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.moyu.system.sys.model.entity.SysScope;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
-* @author shisong
-* @description 针对表【sys_scope(数据权限分组表)】的数据库操作Mapper
-* @createDate 2025-02-27 10:19:59
-* @Entity com.moyu.system.sys.model.entity.SysScope
-*/
+ * @author shisong
+ * @description 针对表【sys_scope(数据权限分组表)】的数据库操作Mapper
+ * @createDate 2025-02-27 10:19:59
+ * @Entity com.moyu.system.sys.model.entity.SysScope
+ */
+@Mapper
 public interface SysScopeMapper extends BaseMapper<SysScope> {
 
 }
