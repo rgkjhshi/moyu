a3b901605220dc88f826de8f8f2d8b5155712b17
shisong <rgkjhshi@gmail.com>
2025-02-27 16:26:25 +0800

    Update CustomDataPermissionHandler.java

--- moyu-common/common-mybatis/src/main/java/com/moyu/common/mybatis/handler/CustomDataPermissionHandler.java
+++ moyu-common/common-mybatis/src/main/java/com/moyu/common/mybatis/handler/CustomDataPermissionHandler.java
@@ -1,6 +1,9 @@
 package com.moyu.common.mybatis.handler;
 
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.extension.plugins.handler.DataPermissionHandler;
@@ -9,8 +12,11 @@ import com.moyu.common.security.util.SecurityUtils;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 
 import java.lang.reflect.Method;
+import java.util.Set;
 
 /**
  * 自定义数据权限处理器
@@ -55,9 +61,20 @@ public class CustomDataPermissionHandler implements DataPermissionHandler {
      * @return 构建后查询条件
      */
     @SneakyThrows
-    public static Expression dataScopeFilter(String orgAlias, String orgColumnName, String userAlias, String userColumnName, Expression where) {
+    public static Expression dataScopeFilter(String orgAlias, String orgColumn, String userAlias, String userColumn, Expression where) {
         // 设置数据权限的语句，可在通过拦截注解此处理，也可不通过注解直接在代码中处理
-        return where;
+        String orgColumnName = StrUtil.isNotBlank(orgAlias) ? (orgAlias + StringPool.DOT + orgColumn) : orgColumn;
+        // 数据权限范围
+        Set<String> scopes = SecurityUtils.getScopes();
+        if (ObjectUtil.isEmpty(scopes)) {
+            return where;
+        }
+        String sqlStr = orgColumnName + " IN ('" + CollectionUtil.join(scopes, "', '") + "')";
+        Expression appendExpression = CCJSqlParserUtil.parseCondExpression(sqlStr);
+        if (where == null) {
+            return appendExpression;
+        }
+        return new AndExpression(where, appendExpression);
     }
 
 }
