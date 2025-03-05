f34c1c395f00e0a1d293a42ffda86fd7ed069f56
shisong <rgkjhshi@gmail.com>
2025-03-06 01:04:14 +0800

    UserInfo

---
+++ moyu-system/src/main/java/com/moyu/system/sys/model/vo/GroupInfo.java
@@ -0,0 +1,36 @@
+package com.moyu.system.sys.model.vo;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 岗位信息视图对象
+ *
+ * @author shisong
+ * @since 2025-03-06
+ */
+@Getter
+@Setter
+@ToString
+@Builder
+public class GroupInfo {
+    /**
+     * 岗位code
+     */
+    private String code;
+    /**
+     * 岗位名称
+     */
+    private String name;
+    /**
+     * 当前组织机构
+     */
+    private String orgCode;
+    /**
+     * 当前组织机构名称
+     */
+    private String orgName;
+}
--- moyu-system/src/main/java/com/moyu/system/sys/model/vo/UserInfo.java
+++ moyu-system/src/main/java/com/moyu/system/sys/model/vo/UserInfo.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -24,34 +25,47 @@ public class UserInfo {
      * 账号
      */
     private String account;
-
-    /**
-     * 昵称
-     */
-    private String nickName;
-
-    /**
-     * 头像
-     */
-    private String avatar;
-
     /**
      * 姓名
      */
     private String name;
-
+    /**
+     * 昵称
+     */
+    private String nickName;
+    /**
+     * 头像
+     */
+    private String avatar;
     /**
      * 用户角色集合 ["root","admin"]
      */
     private Set<String> roles;
-
     /**
      * 用户权限标识集合
      */
     private Set<String> perms;
 
     /**
-     * 用户数据范围集合
+     * 当前组织机构
+     */
+    private String orgCode;
+    /**
+     * 当前岗位(可能为空)
+     */
+    private String groupCode;
+    /**
+     * 数据权限范围
+     */
+    private Integer dataScope;
+    /**
+     * 自定义数据权限集合
      */
     private Set<String> scopes;
+
+    /**
+     * 岗位列表
+     */
+    private List<GroupInfo> groupInfoList;
+
 }
