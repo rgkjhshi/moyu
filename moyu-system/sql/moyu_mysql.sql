ec2ca68385ca4e9263b091ccd2e226cf66a16448
shisong <rgkjhshi@gmail.com>
2025-03-05 15:54:13 +0800

    Update moyu_mysql.sql

--- moyu-system/sql/moyu_mysql.sql
+++ moyu-system/sql/moyu_mysql.sql
@@ -95,22 +95,23 @@ create table sys_role
 drop table if exists sys_group;
 create table sys_group
 (
-    `id`          bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '主键id',
-    `name`        VARCHAR(64)  NULL DEFAULT NULL COMMENT '名称',
-    `code`        VARCHAR(64)  NULL DEFAULT NULL COMMENT '编码',
-    `group_type`  TINYINT(5)   NULL DEFAULT NULL COMMENT '分组类型(字典 1特有 2通用 3自建)',
-    `org_code`    VARCHAR(64)  NULL DEFAULT NULL COMMENT '直属组织编码',
-    `org_name`    VARCHAR(64)  NULL DEFAULT NULL COMMENT '直属组织名称',
+    `id`          bigint(20)    NOT NULL AUTO_INCREMENT COMMENT '主键id',
+    `name`        VARCHAR(64)   NULL DEFAULT NULL COMMENT '名称',
+    `code`        VARCHAR(64)   NULL DEFAULT NULL COMMENT '编码',
+    `data_scope`  TINYINT(5)    NULL DEFAULT NULL COMMENT '数据范围(字典 0无限制 1本人数据 2本机构 3本机构及以下 4自定义)',
+    `scope_set`   VARCHAR(1024) NULL DEFAULT NULL COMMENT '自定义scope集合,逗号分隔',
+    `org_code`    VARCHAR(64)   NULL DEFAULT NULL COMMENT '直属组织编码',
+    `org_name`    VARCHAR(64)   NULL DEFAULT NULL COMMENT '直属组织名称',
 
-    `sort_num`    INT(10)      NULL DEFAULT NULL COMMENT '排序顺序',
-    `status`      TINYINT(5)   NULL DEFAULT 0 COMMENT '状态（0正常 1停用）',
-    `ext_json`    LONGTEXT     NULL COMMENT '扩展信息',
-    `remark`      VARCHAR(200) NULL DEFAULT NULL comment '备注',
-    `delete_flag` TINYINT(5)   NULL DEFAULT 0 COMMENT '删除标志（0未删除  1已删除）',
-    `create_time` DATETIME     NULL DEFAULT NULL COMMENT '创建时间',
-    `create_by`   VARCHAR(32)  NULL DEFAULT NULL COMMENT '创建人',
-    `update_time` DATETIME     NULL DEFAULT NULL COMMENT '修改时间',
-    `update_by`   VARCHAR(32)  NULL DEFAULT NULL COMMENT '修改人',
+    `sort_num`    INT(10)       NULL DEFAULT NULL COMMENT '排序顺序',
+    `status`      TINYINT(5)    NULL DEFAULT 0 COMMENT '状态（0正常 1停用）',
+    `ext_json`    LONGTEXT      NULL COMMENT '扩展信息',
+    `remark`      VARCHAR(200)  NULL DEFAULT NULL comment '备注',
+    `delete_flag` TINYINT(5)    NULL DEFAULT 0 COMMENT '删除标志（0未删除  1已删除）',
+    `create_time` DATETIME      NULL DEFAULT NULL COMMENT '创建时间',
+    `create_by`   VARCHAR(32)   NULL DEFAULT NULL COMMENT '创建人',
+    `update_time` DATETIME      NULL DEFAULT NULL COMMENT '修改时间',
+    `update_by`   VARCHAR(32)   NULL DEFAULT NULL COMMENT '修改人',
     primary key (`id`)
 ) ENGINE = InnoDB
   CHARACTER SET = utf8mb4
