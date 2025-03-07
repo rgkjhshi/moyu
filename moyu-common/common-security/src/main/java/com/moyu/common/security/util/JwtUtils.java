8a913aab9cac8ff9281b7cec37e592255f2f32a5
shisong <rgkjhshi@gmail.com>
2025-03-07 09:59:22 +0800

    Update JwtUtils.java

--- moyu-common/common-security/src/main/java/com/moyu/common/security/util/JwtUtils.java
+++ moyu-common/common-security/src/main/java/com/moyu/common/security/util/JwtUtils.java
@@ -1,6 +1,5 @@
 package com.moyu.common.security.util;
 
-
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.jwt.JWT;
@@ -55,7 +54,7 @@ public class JwtUtils {
         String username = (String) jwt.getPayload(JWTPayload.SUBJECT);
         String orgCode = (String) jwt.getPayload("orgCode");
         String groupCode = (String) jwt.getPayload("groupCode");
-        Integer dataScope = ObjectUtil.isEmpty(jwt.getPayload("dataScope")) ? 1 : ((Number) jwt.getPayload("dataScope")).intValue();
+        Integer dataScope = ObjectUtil.isEmpty(jwt.getPayload("dataScope")) ? null : ((Number) jwt.getPayload("dataScope")).intValue();
         Set<String> scopes = ObjectUtil.isEmpty(jwt.getPayload("scopes")) ? new HashSet<>() : new HashSet<>((List<String>) jwt.getPayload("scopes"));
         Set<String> perms = ObjectUtil.isEmpty(jwt.getPayload("perms")) ? new HashSet<>() : new HashSet<>((List<String>) jwt.getPayload("perms"));
         Set<String> roles = ObjectUtil.isEmpty(jwt.getPayload("roles")) ? new HashSet<>() : new HashSet<>((List<String>) jwt.getPayload("roles"));
