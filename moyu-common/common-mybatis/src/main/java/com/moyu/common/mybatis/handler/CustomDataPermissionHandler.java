0933550a025694169b54a1d9a0fb09f0b554af72
shisong <rgkjhshi@gmail.com>
2025-03-07 09:43:30 +0800

    Update CustomDataPermissionHandler.java

--- moyu-common/common-mybatis/src/main/java/com/moyu/common/mybatis/handler/CustomDataPermissionHandler.java
+++ moyu-common/common-mybatis/src/main/java/com/moyu/common/mybatis/handler/CustomDataPermissionHandler.java
@@ -95,10 +95,11 @@ public class CustomDataPermissionHandler implements MultiDataPermissionHandler {
             case ORG_CHILD: {
                 // 本机构及以下
                 String orgCode = SecurityUtils.getLoginUser().getOrgCode();
-                // 这种处理方式适合所有服务都能访问组织机构表且组织机构不多的情况(否则数量过多会导致in效率低下)
+                // 第一种方法使用IN scopes,这种处理方式适合所有服务都能访问组织机构表且组织机构不多的情况(否则数量过多会导致in效率低下)
                 // sqlStr = orgColumn + " IN ( SELECT code FROM sys_org WHERE code = '" + orgCode + "' OR find_in_set( '" + orgCode + "' , org_path ) )";
-                // 第二种处理方式则需要在数据表中新增一个表示组织机构树层级路径的字段，如:org_path(组织机构若变更则要洗数据)
+                // 第二种方法使用 orgPath Like, 这种处理方式则需要在数据表中新增一个表示组织机构树层级路径的字段，如:org_path(组织机构若变更则要洗数据)
                 // sqlStr = "( " + orgColumn + " = '" + orgCode + "' OR " + annotation.orgPathColumn() + " LIKE '%" + orgCode + "%' )";
+                // 第三种方法使用 orgPath find_in_set, 这种处理方式与like类似但效率高点
                 sqlStr = "( " + orgColumn + " = '" + orgCode + "' OR find_in_set('" + orgCode + "', " + annotation.orgPathColumn() + " ) )";
                 break;
             }
