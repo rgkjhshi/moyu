41d62ee8dd9d5600f3ecb884e501ba0301ce257a
shisong <rgkjhshi@gmail.com>
2025-03-11 11:23:08 +0800

    Update AuthController.java

--- moyu-system/src/main/java/com/moyu/system/auth/controller/AuthController.java
+++ moyu-system/src/main/java/com/moyu/system/auth/controller/AuthController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 登陆控制器
@@ -31,8 +30,7 @@ public class AuthController {
      * 用户登陆
      */
     @PostMapping("/login")
-    public BaseResponse<String> userLogin(HttpServletRequest request, @Validated UserLoginParam loginParam) {
-//        String IpStr = ServletUtil.getClientIP(request);
+    public BaseResponse<String> userLogin(@Validated UserLoginParam loginParam) {
         String token = loginService.login(loginParam);
         return BaseResponse.getSuccessResponse(token);
     }
